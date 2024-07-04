28216ed83449a1da841de19926c69c62a5974224
hmxs <hmxsqaq@163.com>
2024-07-04 18:19:10 +0800

    extract GenerateNoiseMap to NoiseUtility.cs; clean some code;

---
+++ Assets/Scripts/NoiseUtility.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace ProceduralGeneration
+{
+    public static class NoiseUtility
+    {
+        public static float[,] GetNoiseMap(int seed, int width, int height, float scale)
+        {
+            Random.InitState(seed);
+            // init map
+            float[,] noiseMap = new float[width, height];
+            // avoid scale <= 0
+            scale = scale <= 0 ? 0.0001f : scale;
+            // get offset point
+            Vector2 offset = new Vector2(Random.Range(-9999f, 9999f), Random.Range(-9999f, 9999));
+            // store min and max value for normalization
+            float minNoise = float.MaxValue;
+            float maxNoise = float.MinValue;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    float sampleX = x / scale + offset.x;
+                    float sampleY = y / scale + offset.y;
+                    float noise = PerlinNoise.GetNoise(sampleX, sampleY);
+                    // get min and max noise value to lerp
+                    if (noise < minNoise) minNoise = noise;
+                    if (noise > maxNoise) maxNoise = noise;
+                    noiseMap[x, y] = noise;
+                }
+            }
+
+            // normalize the noise map
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    noiseMap[x, y] = Mathf.InverseLerp(minNoise, maxNoise, noiseMap[x, y]);
+
+            return noiseMap;
+        }
+    }
+}
--- Assets/Scripts/TilemapGenerator.cs
+++ Assets/Scripts/TilemapGenerator.cs
@@ -2,7 +2,6 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using Random = UnityEngine.Random;
 
 namespace ProceduralGeneration
 {
@@ -31,18 +30,17 @@ namespace ProceduralGeneration
         [SerializeField] [HideIf("mapType", MapType.Gray)] [OnValueChanged("GenerateMap")]
         private bool singleWaterElimination;
         [SerializeField] [OnValueChanged("GenerateMap")]
-        [Range(0f, 0.5f)] private float lacunarity;
+        [Range(0.001f, 50f)] private float scale;
 
         private float[,] _noiseMap;
-        private float _minNoise = float.MaxValue;
-        private float _maxNoise = float.MinValue;
-        private float WaterThreshold => Mathf.Lerp(_minNoise, _maxNoise, waterProbability);
 
         [Title("Run")]
         [Button]
         public void GenerateMap()
         {
-            GenerateNoiseMap();
+            if (useRandomSeed) seed = Time.time.GetHashCode();
+            _noiseMap = NoiseUtility.GetNoiseMap(seed, width, height, scale);
+
             ClearTiles();
             switch (mapType)
             {
@@ -58,48 +56,31 @@ namespace ProceduralGeneration
             }
         }
 
-        private void GenerateNoiseMap()
-        {
-            if (useRandomSeed) seed = Time.time.GetHashCode();
-            Random.InitState(seed);
-            // init map
-            _noiseMap = new float[width, height];
-            // get random offset which will be used as the start point to generate noise map
-            float offset = Random.Range(-9999f, 9999f);
-
-            for (int w = 0; w < width; w++)
-            for (int h = 0; h < height; h++)
-            {
-                float noise = PerlinNoise.GetNoise(w * lacunarity + offset, h * lacunarity + offset);
-                // get min and max noise value to lerp
-                if (noise < _minNoise) _minNoise = noise;
-                if (noise > _maxNoise) _maxNoise = noise;
-                _noiseMap[w, h] = noise;
-            }
-        }
-
         private void ClearTiles() => tilemap.ClearAllTiles();
 
         private void SetGrayTiles()
         {
             for (int w = 0; w < width; w++)
-            for (int h = 0; h < height; h++)
             {
-                tilemap.SetTile(new Vector3Int(w, h, 0), squareTile);
-                var colorValue = Mathf.InverseLerp(_minNoise, _maxNoise, _noiseMap[w, h]);
-                var color = Color.Lerp(Color.black, Color.white, colorValue);
-                tilemap.SetTileFlags(new Vector3Int(w, h, 0), TileFlags.None);
-                tilemap.SetColor(new Vector3Int(w, h, 0), color);
+                for (int h = 0; h < height; h++)
+                {
+                    tilemap.SetTile(new Vector3Int(w, h, 0), squareTile);
+                    var color = Color.Lerp(Color.black, Color.white, _noiseMap[w, h]);
+                    tilemap.SetTileFlags(new Vector3Int(w, h, 0), TileFlags.None);
+                    tilemap.SetColor(new Vector3Int(w, h, 0), color);
+                }
             }
         }
 
         private void SetWaterTiles()
         {
             for (int w = 0; w < width; w++)
-            for (int h = 0; h < height; h++)
             {
-                var tile = _noiseMap[w, h] < WaterThreshold ? waterTile : grassTile;
-                tilemap.SetTile(new Vector3Int(w, h, 0), tile);
+                for (int h = 0; h < height; h++)
+                {
+                    var tile = _noiseMap[w, h] < waterProbability ? waterTile : grassTile;
+                    tilemap.SetTile(new Vector3Int(w, h, 0), tile);
+                }
             }
         }
 
@@ -109,11 +90,13 @@ namespace ProceduralGeneration
             {
                 bool hasSingleWater = false;
                 for (int w = 0; w < width; w++)
-                for (int h = 0; h < height; h++)
                 {
-                    if (!(_noiseMap[w, h] < WaterThreshold) || !CheckAroundHavePairLand(w, h)) continue;
-                    _noiseMap[w, h] = 1;
-                    hasSingleWater = true;
+                    for (int h = 0; h < height; h++)
+                    {
+                        if (!(_noiseMap[w, h] < waterProbability) || !CheckAroundHavePairLand(w, h)) continue;
+                        _noiseMap[w, h] = 1;
+                        hasSingleWater = true;
+                    }
                 }
 
                 if (!hasSingleWater) break;
@@ -123,10 +106,10 @@ namespace ProceduralGeneration
         private bool CheckAroundHavePairLand(int x, int y)
         {
             bool left = false, right = false, up = false, down = false;
-            if (x > 0) left = _noiseMap[x - 1, y] > WaterThreshold;
-            if (x < width - 1) right = _noiseMap[x + 1, y] > WaterThreshold;
-            if (y > 0) up = _noiseMap[x, y - 1] > WaterThreshold;
-            if (y < height - 1) down = _noiseMap[x, y + 1] > WaterThreshold;
+            if (x > 0) left = _noiseMap[x - 1, y] > waterProbability;
+            if (x < width - 1) right = _noiseMap[x + 1, y] > waterProbability;
+            if (y > 0) up = _noiseMap[x, y - 1] > waterProbability;
+            if (y < height - 1) down = _noiseMap[x, y + 1] > waterProbability;
             return (left && right) || (up && down);
         }
     }
